7cbb56f65ec9ed7e61043e1f7574484a79bbe7a2
renatacovisi <renatacovisi@hotmail.com>
2020-05-04 15:32:20 +0100

    home-v1-2 - with header applied with php

---
+++ app/views/header.php
@@ -0,0 +1,52 @@
+<header>
+            <nav class="navbar navbar-expand-lg fixed-top shadow">
+                <div class="col-2">
+                    <a class="navbar-brand" href="#"><img id="logo" src="../app/images/logo2.svg" alt=""></a>
+                </div>
+                <div class="col-10">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                    <span><i id="hamburgerButton" class="fas fa-bars"></i></span>
+                </button>
+                <div class="collapse navbar-collapse sticky-top text-right" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="aboutUs.php">About us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="classes.php">Classes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="testimonials.php">Testimonials</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle fColorYellow" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Site map
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item fColorYellow" href="index.php">Home</a>
+                                <a class="dropdown-item fColorYellow" href="aboutUs.php">About us</a>
+                                <a class="dropdown-item fColorYellow" href="faq.php">FAQ</a>
+                            </div>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="contacts.php">Contact us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link fColorYellow" href="gallery.php">Gallery</a>
+                        </li>
+                    </ul>
+                    <div class="inlineBlock float-right">
+                        <button type="button" class="fColorIndigo btn btn-light m-1 ml-3 inlineBlock buttonSize">Sign
+                            Up</button>
+                        <button type="button" class="fColorIndigo btn btn-light m-1 ml-3 inlineBlock buttonSize">Sign
+                            In</button>
+                    </div>
+                </div>
+            </div>
+            </nav>
+        </header>
