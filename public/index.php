7ce4d1149871b2d555b2de94d119319eb55d7993
renatacovisi <renatacovisi@hotmail.com>
2020-05-28 08:59:17 +0100

    feature adm edit home with comments

--- public/index.php
+++ public/index.php
@@ -1,54 +1,68 @@
 <?php
+//using real path to make easier to the scripts to find the other files
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-#Include the config file - configuration settings are available to the script
+//require config file
 require_once("$root/Fitness-Center-project/public/config.php");
+//require post class to allow control posts transactions
 require_once("../classes/Post.php");
 
+//sets the action that is being received from the browser for all interactions
 $action = isset($_GET['action']) ? $_GET['action'] : "";
+//initializes the results variable with an array
 $results = array();
+//initializes the status message that is being received from the browser for all interactions
 $status = isset($_GET['status']) ? $_GET['status'] : "";
+
+//initializes the id of a post that is being received from the browser for all interactions
 $postId = isset($_GET['id']) ? $_GET['id'] : "";
+
+// Gets a pot by the id and assign it to a position on the results variable
 $results['postToEdit'] = Post::getById($postId);
+
+// if the post does not have an id it creates an empty post to help to handle it in the creation and edition
 if ($results['postToEdit'] == null) {
     $results['postToEdit'] = new Post();
 }
 
-
+// indicates if the actions bellow are true or false to allow the modals to be open
 $results['confirmLogout'] = $action == "confirmLogout";
 
 $results['showEditPosts'] = ($action == "showEditPosts" || $action == 'savePostResult' || $action == 'editPost' || $action == 'deletePost' );
 
-$results['showEditCarousel'] = ($action == "showEditCarousel" || $action == 'savePostResult');
+//$results['showEditCarousel'] = ($action == "showEditCarousel" || $action == 'savePostResult');
 
+//verify if the upload and saving are ok and display information to the user
 if ($action == 'savePostResult' and $status == 'uploadSuccess') {
     $results['message'] = "Changes Saved!";
 } elseif ($action == 'savePostResult' and $status == 'uploadFailed') {
     $results['message'] = "An error occurred, please try again.";
 }
 
-if ($action == 'saveCarouselResult' and $status == 'uploadSuccess') {
-    $results['message'] = "Changes Saved!";
-} elseif ($action == 'saveCarouselResult' and $status == 'uploadFailed') {
-    $results['message'] = "An error occurred, please try again.";
-}
+//if ($action == 'saveCarouselResult' and $status == 'uploadSuccess') {
+//    $results['message'] = "Changes Saved!";
+//} elseif ($action == 'saveCarouselResult' and $status == 'uploadFailed') {
+//    $results['message'] = "An error occurred, please try again.";
+//}
 
+// verify if the action is edit post and there is no id and display a message
 if ($action == 'editPost') {
     if ($results['postToEdit']->id == null ) {
         $results['message'] = 'Post Not Found';
     }
+//    if the there is an id stores the new values in the post object and update it
     else {
-    #stores the new values in the Article object
         $results['postToEdit']->storeFormValues( $_POST );
-    #saves the changed object by calling update()
         $results['postToEdit']->update();
         $results['message'] = "Changes Saved!";
     }
 }
 
+// if the action is delete post and there is no id display a message,
 if ($action == 'deletePost') {
     if ($postId == '' ) {
         $results['message'] = 'Post Not Found';
     }
+//    if there is an id retrieves it from the database, delete it and display a message
     else {
         $postToDelete = Post::getById($postId);
         $postToDelete->delete();
@@ -56,83 +70,22 @@ if ($action == 'deletePost') {
     }
 }
 
+// displays the homepage
 homePage($results);
 
-function homePage($results)
-{
+// function to create the home page with the posts information
+function homePage($results) {
 
-#calls the getList() method of the Article class
+//    stores the posts of type news in a variable and the type offers in another
     $data1 = Post::getList(2, "news");
     $data2 = Post::getList(2, "offers");
-#stores the results, along with the page title, in a $results associative array
-#so the template can display them in the page
+
+//    assign the values of each variable for a postion in the results array
     $results['news'] = $data1['results'];
     $results['offers'] = $data2['results'];
 
+    //starts the display of the home page
     require("homePage.php");
 }
 
-
 ?>
-
-
-<!--//#check that the $_GET['action'] value exists by using isset-->
-<!--//#If it doesn’t, we set the corresponding $action variable to an empty string ("")-->
-<!--//#ternary operator provides a shorthand way of writing the if...else statements-->
-<!--//#The ternary operator is represented by the question mark (?) symbol-->
-<!--//#it takes three operands: a condition to check, a result for true, and a result for false.-->
-<!--//$action = isset( $_GET['action'] ) ? $_GET['action'] : "";-->
-<!--//#looks at the action parameter in the URL to determine which action to perform-->
-<!--//#(display the archive, or view an article)-->
-<!--//#If no action parameter is in the URL then the script displays the site homepage-->
-<!--//switch ( $action ) {-->
-<!--//    case 'archive':-->
-<!--//        archive();-->
-<!--//        break;-->
-<!--//    case 'viewArticle':-->
-<!--//        viewArticle();-->
-<!--//        break;-->
-<!--//    default:-->
-<!--//        homepage();-->
-<!--//}-->
-<!--//-->
-<!--//#displays a list of all the articles in the database-->
-<!--//function archive() {-->
-<!--//    $results = array();-->
-<!--//    #calls the getList() method of the Article class-->
-<!--//    $data = Article::getList();-->
-<!--//    #stores the results, along with the page title, in a $results associative array-->
-<!--//    #so the template can display them in the page-->
-<!--//    $results['articles'] = $data['results'];-->
-<!--//    $results['totalRows'] = $data['totalRows'];-->
-<!--//    $results['pageTitle'] = "Article Archive | Widget News";-->
-<!--//    #include the path to the template file to display the page.-->
-<!--//    require( TEMPLATE_PATH . "/archive.php" );-->
-<!--//}-->
-<!--//#displays a single article page-->
-<!--//function viewArticle() {-->
-<!--//    #retrieves the ID of the article to display from the articleId URL parameter-->
-<!--//    if ( !isset($_GET["articleId"]) || !$_GET["articleId"] ) {-->
-<!--//        homepage();-->
-<!--//        return;-->
-<!--//    }-->
-<!--//-->
-<!--//    $results = array();-->
-<!--//    #calls the Article class’s getById() method to retrieve the article object-->
-<!--//    #which it stores in the $results array for the template to use.-->
-<!--//    $results['article'] = Article::getById( (int)$_GET["articleId"] );-->
-<!--//    $results['pageTitle'] = $results['article']->title . " | Widget News";-->
-<!--//    require( TEMPLATE_PATH . "/viewArticle.php" );-->
-<!--//}-->
-<!--//#displays the site homepage containing a list of up to HOMEPAGE_NUM_ARTICLES articles-->
-<!--//#it passes HOMEPAGE_NUM_ARTICLES to the getList() method to limit the number of articles returned-->
-<!--//function homepage() {-->
-<!--//    $results = array();-->
-<!--//    $data = Article::getList( HOMEPAGE_NUM_ARTICLES );-->
-<!--//    $results['articles'] = $data['results'];-->
-<!--//    $results['totalRows'] = $data['totalRows'];-->
-<!--//    $results['pageTitle'] = "Widget News";-->
-<!--//    require( TEMPLATE_PATH . "/homepage.php" );-->
-<!--//}-->
-<!--//-->
-<!--//?>-->
