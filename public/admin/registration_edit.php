d6becf256e7728d3f96e7f918e54ef5ad2544de4
renatacovisi <renatacovisi@hotmail.com>
2020-05-29 16:49:01 +0100

    starting registration management part

--- public/admin/registration_edit.php
+++ public/admin/registration_edit.php
@@ -13,7 +13,7 @@ require_once(FIXED_PATH."/Fitness-Center-Project/app/src/session.php");
 $postToEdit = $results['postToEdit'];
 
 //if the action is to show the edit posts modal
-if ($results['showEditPosts']) {
+if ($results['showEditFeePlans']) {
     ?>
     <div class="modal fade" id="logoutConfirmation" tabindex="-1" role="dialog" aria-labelledby="logoutConfirmation"
          aria-hidden="true">
@@ -34,37 +34,31 @@ if ($results['showEditPosts']) {
 
                     <!-- sets the correct url to be called in the form depending on the existence of an id that means that is an edition or non existence that means an creation-->
                     <?php
-                    $url = isset($postToEdit->id) ? '/Fitness-Center-Project/public/index.php?action=editPost&id=' . $postToEdit->id : '/Fitness-Center-Project/app/src/upload.php?action=savePost'
+                    $url = isset($feePlanToEdit->id) ? '/Fitness-Center-Project/public/registration.php?action=editFeePlan&id=' . $feePlanToEdit->id : '/Fitness-Center-Project/public/registration.php?action=action=saveFeePlan'
                     ?>
                     <!-- inserts the url in the action of the form-->
-                    <form action="<?php echo $url ?>" method="post" enctype="multipart/form-data">
+                    <form action="<?php echo $url ?>" method="post">
                         <div class="form-group">
                             <!-- uses the id input only if there is an id already set and uses the id in its value-->
-                            <?php if(isset($postToEdit->id)) {?> <input type="hidden" id="id" name="id" value="<?php echo $postToEdit->id?>"> <?php } ?>
-                            <!-- hides the label and input of the image if already exist a post - feature to be done in the future to allow user to change photo as well-->
-                            <label <?php echo isset($postToEdit->id) ? 'hidden' : '' ?> class="font-weight-bold mt-1" for="fileToUpload">Select image
-                                to upload:</label>
-                            <input type="<?php echo isset($postToEdit->id) ? 'hidden' : 'file' ?>" name="fileToUpload" id="fileToUpload" class="d-block"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->photoLink : '' ?>">
+                            <?php if(isset($feePlanToEdit->id)) {?> <input type="hidden" id="id" name="id" value="<?php echo $feePlanToEdit->id?>"> <?php } ?>
+
                             <!--  all inputs allow the admin to edit if a id is there-->
-                            <label class="font-weight-bold mt-3" for="publicationDate">Publication Date</label>
-                            <input type="date" class="form-control" id="publicationDate" name="publicationDate"
-                                   value="<?php echo isset($postToEdit->id) ? date("Y-m-d", $postToEdit->publicationDate) : '' ?>">
-                            <label class="font-weight-bold mt-1" for="title">Title</label>
-                            <input type="text" class="form-control" id="title" name="title"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->title : '' ?>">
-                            <label class="font-weight-bold mt-1" for="text">Text</label>
+                            <label class="font-weight-bold mt-3" for="name">name</label>
+                            <input type="date" class="form-control" id="name" name="name"
+                                   value="<?php echo isset($feePlanToEdit->id) ? $feePlanToEdit->name : '' ?>">
+
+                            <label class="font-weight-bold mt-1" for="text">text</label>
                             <input type="text" class="form-control" id="text" name="text"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->text : '' ?>">
-                            <label class="font-weight-bold mt-1" for="link">Link</label>
-                            <input type="text" class="form-control" id="link" name="link"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->link : '' ?>">
-                            <label class="font-weight-bold mt-1" for="type">Type</label>
+                                   value="<?php echo isset($feePlanToEdit->id) ? $feePlanToEdit->text : '' ?>">
+
+                            <label class="font-weight-bold mt-1" for="price">price</label>
+                            <input type="text" class="form-control" id="price" name="price"
+                                   value="<?php echo isset($feePlanToEdit->id) ? $feePlanToEdit->price : '' ?>">
+
+                            <label class="font-weight-bold mt-1" for="maxClasses">maxClasses</label>
                             <input type="text" class="form-control" id="type" name="type"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->type : '' ?>">
-                            <label class="font-weight-bold mt-1" for="buttonText">Button Text</label>
-                            <input type="text" class="form-control" id="buttonText" name="buttonText"
-                                   value="<?php echo isset($postToEdit->id) ? $postToEdit->buttonText : '' ?>">
+                                   value="<?php echo isset($feePlanToEdit->id) ? $feePlanToEdit->type : '' ?>">
+
                         </div>
                         <input type="submit" value="Save" name="submitPost"
                                class="m-2 btn btn-secondary buttonSizeAdm">
@@ -81,22 +75,17 @@ if ($results['showEditPosts']) {
                             </button>
                             <div class="dropdown-menu" aria-labelledby="dropdownEdit">
                                 <?php
-                                $data1 = Post::getList(50, "news");
-                                $data2 = Post::getList(50, "offers");
+                                $data1 = Fee::getList(50, "news");
                                 foreach ($data1['results'] as $post) { ?>
                                     <a role="button" class="dropdown-item"
-                                       href="<?php echo WEB_URL_PREFIX."/Fitness-Center-Project/public/index.php?action=showEditPosts&id=<?php echo $post->id ?>" ?>"><?php echo $post->title ?></a>
-                                <?php } ?>
-                                <?php foreach ($data2['results'] as $post) { ?>
-                                    <a role="button" class="dropdown-item"
-                                       href="<?php echo WEB_URL_PREFIX."/Fitness-Center-Project/public/index.php?action=showEditPosts&id=<?php echo $post->id ?>" ?>"><?php echo $post->title ?></a>
+                                       href="<?php echo WEB_URL_PREFIX."/Fitness-Center-Project/public/registration.php?action=showEditFeePlans&id=" . $post->id?>"><?php echo $post->title ?></a>
                                 <?php } ?>
                             </div>
                         </div>
                         <!--                        delete button that triggers a confirmation dialog and deletes the selected post-->
                         <a class="btn btn-secondary mt-2 buttonSizeAdm"
-                           href="<?php echo WEB_URL_PREFIX."/Fitness-Center-Project/public/index.php?action=deletePost&id=<?php echo $postToEdit->id ?>" ?>"
-                           role="button" onclick="return confirm('Delete This Article?')">Delete</a>
+                           href="<?php echo WEB_URL_PREFIX."/Fitness-Center-Project/public/registration.php?action=deleteFeePlan&id=" . $postToEdit->id ?>"
+                           role="button" onclick="return confirm('Delete This Fee Plan?')">Delete</a>
                     </div>
 
                 </div>
